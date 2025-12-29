7c7db3d08061d7b30a06055ad174c9f318d5b193
system4trading <104557852+system4trading@users.noreply.github.com>
2025-12-29 20:39:28 +0530

    Update health_check.js

--- scripts/health_check.js
+++ scripts/health_check.js
@@ -1,6 +1,6 @@
 import fs from "fs";
 
-const nav = JSON.parse(fs.readFileSync("data/amfi/nav_120503.json"));
+const nav = JSON.parse(fs.readFileSync("amfi/nav_120503.json"));
 let gaps = 0;
 
 for (let i = 1; i < nav.length; i++) {
