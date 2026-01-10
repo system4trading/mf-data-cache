d42f9424fda2243d5b5e2c0a9a5d4109cb746323
system4trading <104557852+system4trading@users.noreply.github.com>
2026-01-11 03:56:56 +0530

    Update export_to_csv.sql

--- etl/export_to_csv.sql
+++ etl/export_to_csv.sql
@@ -1,29 +1,3 @@
-COPY (
-  SELECT
-    amc_code,
-    amc_name
-  FROM amc
-) TO 'export/amc.csv'
-WITH (HEADER, DELIMITER ',');
-
-COPY (
-  SELECT
-    scheme_code,
-    scheme_name,
-    category,
-    sub_category,
-    amc_code,
-    launch_date,
-    isin
-  FROM mf_schemes
-) TO 'export/mf_schemes.csv'
-WITH (HEADER, DELIMITER ',');
-
-COPY (
-  SELECT
-    scheme_code,
-    nav_date,
-    nav
-  FROM mf_nav_history
-) TO 'export/mf_nav_history.csv'
-WITH (HEADER, DELIMITER ',');
+COPY amc TO 'export/amc.csv' (HEADER, DELIMITER ',');
+COPY mf_schemes TO 'export/mf_schemes.csv' (HEADER, DELIMITER ',');
+COPY mf_nav_history TO 'export/mf_nav_history.csv' (HEADER, DELIMITER ',');
