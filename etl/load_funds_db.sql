2c56ae247c4c6d1b7c9427ecfb0bba01259cdc30
system4trading <104557852+system4trading@users.noreply.github.com>
2026-01-11 04:26:20 +0530

    Update load_funds_db.sql

--- etl/load_funds_db.sql
+++ etl/load_funds_db.sql
@@ -3,11 +3,28 @@ LOAD sqlite;
 
 ATTACH 'funds.db' AS mf (TYPE sqlite);
 
-CREATE TABLE amc AS
-SELECT DISTINCT amc_code, amc_name
-FROM mf.amcs;
+-- ======================
+-- AMC
+-- ======================
+INSERT INTO amc (amc_code, amc_name)
+SELECT
+  amc_code,
+  amc_name
+FROM mf.amc
+ON CONFLICT DO NOTHING;
 
-CREATE TABLE mf_schemes AS
+-- ======================
+-- Schemes
+-- ======================
+INSERT INTO mf_schemes (
+  scheme_code,
+  scheme_name,
+  category,
+  sub_category,
+  amc_code,
+  launch_date,
+  isin
+)
 SELECT
   scheme_code,
   scheme_name,
@@ -16,11 +33,20 @@ SELECT
   amc_code,
   launch_date,
   isin
-FROM mf.schemes;
+FROM mf.scheme
+ON CONFLICT (scheme_code) DO NOTHING;
 
-CREATE TABLE mf_nav_history AS
+-- ======================
+-- NAV history
+-- ======================
+INSERT INTO mf_nav_history (
+  scheme_code,
+  nav_date,
+  nav
+)
 SELECT
   scheme_code,
-  date AS nav_date,
+  date,
   nav
-FROM mf.nav;
+FROM mf.nav
+ON CONFLICT (scheme_code, nav_date) DO NOTHING;
