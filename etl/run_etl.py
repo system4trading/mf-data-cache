2df47c17e47b1013c878004ff39bbbcbf3e7b4a2
system4trading <104557852+system4trading@users.noreply.github.com>
2026-01-18 00:22:42 +0530

    Update run_etl.py

--- etl/run_etl.py
+++ etl/run_etl.py
@@ -1,13 +1,14 @@
 import duckdb
-import pathlib
+from pathlib import Path
 
 con = duckdb.connect("analytics.duckdb")
 
 sql_files = [
+    "etl/duckdb_schema.sql",        # 👈 MUST be first
     "etl/load_amfi_master.sql",
-    "etl/load_nav.sql",
+    "etl/load_nav_history.sql",
     "etl/transform.sql",
-    "etl/export_to_csv.sql",
+    "etl/export_to_csv.sql"
 ]
 
 for sql in sql_files:
