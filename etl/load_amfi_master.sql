fe442286170a8609a3452161dfe219d20763a5f0
system4trading <104557852+system4trading@users.noreply.github.com>
2026-01-18 19:39:11 +0530

    Update load_amfi_master.sql

--- etl/load_amfi_master.sql
+++ etl/load_amfi_master.sql
@@ -1,27 +1,19 @@
--- Read raw NAVAll.txt (no header, semicolon separated)
-CREATE OR REPLACE TEMP TABLE navall_raw AS
-SELECT *
-FROM read_csv(
-  'raw/navall/NAVAll.txt',
-  delim=';',
-  header=false,
-  quote='',
-  escape='',
-  nullstr='',
-  ignore_errors=true
-);
-
--- Normalize into raw table
-INSERT INTO amfi_master_raw (
-  scheme_code,
-  isin_payout,
-  isin_reinvest,
-  scheme_name
-)
+-- Read raw AMFI master
+CREATE OR REPLACE TABLE amfi_master_raw AS
 SELECT
-  CAST(column0 AS INTEGER)            AS scheme_code,
-  NULLIF(column1, '')                 AS isin_payout,
-  NULLIF(column2, '')                 AS isin_reinvest,
-  TRIM(column3)                       AS scheme_name
-FROM navall_raw
-WHERE column0 ~ '^[0-9]+$';
+  column0::INTEGER          AS scheme_code,
+  column1                  AS isin_payout,
+  column2                  AS isin_reinvest,
+  column3                  AS scheme_name,
+  column4                  AS amc_name,
+  column5                  AS scheme_type,
+  column6                  AS scheme_category,
+  column7                  AS plan,
+  column8                  AS option,
+  STRPTIME(column9, '%d-%b-%Y')::DATE AS launch_date
+FROM read_csv(
+  'raw/amfi/MFMaster.txt',
+  delim='|',
+  header=false
+)
+WHERE column0 IS NOT NULL;
