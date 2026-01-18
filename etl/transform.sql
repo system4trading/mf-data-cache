e25f28baecfe1b831259492225a4968f394fe9af
system4trading <104557852+system4trading@users.noreply.github.com>
2026-01-18 19:53:55 +0530

    Update transform.sql

--- etl/transform.sql
+++ etl/transform.sql
@@ -1,23 +1,36 @@
 -- AMC
-CREATE OR REPLACE TABLE amc AS
+DELETE FROM amc;
+
+INSERT INTO amc
 SELECT
-  dense_rank() OVER (ORDER BY amc_name) AS amc_code,
+  dense_rank() OVER (ORDER BY amc_name),
   amc_name
 FROM (
-  SELECT DISTINCT amc_name
-  FROM amfi_master_raw
+  SELECT DISTINCT amc_name FROM amfi_master_raw
 ) t;
 
 -- Schemes
-CREATE OR REPLACE TABLE mf_schemes AS
+DELETE FROM mf_schemes;
+
+INSERT INTO mf_schemes
 SELECT
   r.scheme_code,
   r.scheme_name,
   r.scheme_type,
-  r.scheme_category AS category,
+  r.scheme_category,
   r.plan,
   r.option,
   a.amc_code,
   r.launch_date
 FROM amfi_master_raw r
-JOIN amc a USING (amc_name);
+JOIN amc a ON r.amc_name = a.amc_name;
+
+-- NAV history
+DELETE FROM mf_nav_history;
+
+INSERT INTO mf_nav_history
+SELECT
+  scheme_code,
+  nav_date,
+  nav
+FROM nav_raw;
