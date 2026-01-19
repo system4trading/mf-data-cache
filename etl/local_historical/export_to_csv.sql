06a345e405acdb96c4e5bcda6f778068c6da9e07
system4trading <104557852+system4trading@users.noreply.github.com>
2026-01-19 18:01:25 +0530

    Delete etl/local_historical directory

--- etl/local_historical/export_to_csv.sql
+++
@@ -1,7 +0,0 @@
--- =========================================
--- DuckDB: Export NAV data to CSV
--- =========================================
-
-COPY nav_clean
-TO 'export/mf_nav_history.csv'
-(HEADER true, DELIMITER ',');
