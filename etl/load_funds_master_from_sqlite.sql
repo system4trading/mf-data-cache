c7b8e6e895ef878dc6f1d7cfd08849392418b9f4
system4trading <104557852+system4trading@users.noreply.github.com>
2026-01-19 13:57:45 +0530

    Update load_funds_master_from_sqlite.sql

--- etl/load_funds_master_from_sqlite.sql
+++ etl/load_funds_master_from_sqlite.sql
@@ -1,13 +1,11 @@
 -- ---------------------------------------------------------
 -- Load AMFI Scheme Master from captn3m0 funds.db (SQLite)
--- Source: https://github.com/captn3m0/historical-mf-data
 -- ---------------------------------------------------------
 
--- Attach the SQLite database
 ATTACH 'funds.db' AS src (TYPE SQLITE);
 
 ------------------------------------------------------------
--- 1. AMC MASTER
+-- AMC MASTER
 ------------------------------------------------------------
 INSERT INTO core.amc (amc_code, amc_name)
 SELECT DISTINCT
@@ -18,7 +16,7 @@ WHERE amc_code IS NOT NULL
   AND amc_name IS NOT NULL;
 
 ------------------------------------------------------------
--- 2. MF SCHEME MASTER
+-- MF SCHEME MASTER
 ------------------------------------------------------------
 INSERT INTO core.mf_schemes (
     scheme_code,
@@ -43,10 +41,9 @@ FROM src.schemes s
 WHERE s.scheme_code IS NOT NULL;
 
 ------------------------------------------------------------
--- 3. Sanity checks (visible in DuckDB logs)
+-- Sanity logs
 ------------------------------------------------------------
-SELECT 'amc_loaded' AS table, COUNT(*) FROM core.amc;
-SELECT 'schemes_loaded' AS table, COUNT(*) FROM core.mf_schemes;
+SELECT 'amc_loaded', COUNT(*) FROM core.amc;
+SELECT 'schemes_loaded', COUNT(*) FROM core.mf_schemes;
 
--- Cleanup
 DETACH src;
